2c1b08bec428b01936b23a481706f0dece873ff2
T0MLE <lelaurain.tom@gmail.com>
2023-05-11 20:28:39 +0200

    created match or pass buttons for countries

--- frontend/src/pages/Countries.jsx
+++ frontend/src/pages/Countries.jsx
@@ -2,17 +2,58 @@ import { useEffect, useState } from "react";
 import "../Countries.scss";
 import heart from "../assets/Vector.png";
 import cross from "../assets/croix.png";
+import { Link } from "react-router-dom";
 
 function Countries() {
   const [pictures, setPictures] = useState([]);
   const [countries, setCountries] = useState([]);
   const [randomCountry, setRandomCountry] = useState([]);
+  const [selectedCountry, setSelectedCountry] = useState([]);
+  const [randomIndex, setRandomIndex] = useState();
+
+  const handleclick = () => {
+    localStorage.setItem("selectedCountry", selectedCountry);
+  };
+
+  const handlePass = () => {
+    console.log("index", randomIndex);
+    console.log("li", countries[randomIndex]);
+    countries.splice(randomIndex, 1);
+    setRandomIndex(Math.floor(Math.random() * countries.length));
+    setRandomCountry(countries[randomIndex]?.name.common);
+  };
+
+  const handleMatch = () => {
+    setSelectedCountry([...selectedCountry, randomCountry]);
+    setRandomIndex(Math.floor(Math.random() * countries.length));
+    countries.splice(randomIndex, 1);
+    setRandomCountry(countries[randomIndex]?.name.common);
+  };
 
   useEffect(() => {
+    const localRegion = localStorage.getItem("region");
+    const dataLocal = JSON.parse(localRegion);
     fetch(" https://restcountries.com/v3.1/all")
       .then((response) => response.json())
       .then((response) => {
-        setCountries(response);
+        const arr = [];
+        for (let i = 0; i < dataLocal.length; i++) {
+          const filterRegion = response.filter(
+            (e) =>
+              e.population >= 10000000 &&
+              e.continents[0] === dataLocal[i].region
+          );
+          arr.push(filterRegion);
+        }
+        console.log("arr", arr);
+        const newArr = [];
+        for (let i = 0; i < arr.length; i++) {
+          for (let j = 0; j < arr[i].length; j++) {
+            newArr.push(arr[i][j]);
+          }
+        }
+        setRandomIndex(Math.floor(Math.random() * newArr.length));
+        setCountries(newArr);
       })
       .catch((err) => console.error(err));
   }, []);
@@ -29,14 +70,8 @@ function Countries() {
   }, [randomCountry]);
 
   useEffect(() => {
-    setRandomCountry(
-      countries[Math.floor(Math.random() * countries.length)]?.name.common
-    );
+    setRandomCountry(countries[randomIndex]?.name?.common);
   }, [countries.length]);
-
-  useEffect(() => {
-    console.warn(randomCountry);
-  }, [randomCountry]);
 
   return (
     <div className="countries-container">
@@ -48,13 +83,18 @@ function Countries() {
       </div>
       <p>{randomCountry}</p>
       <div className="btn-match">
-        <div className="pass">
+        <div className="pass" onClick={handlePass}>
           <img src={cross} alt="" />
         </div>
-        <div className="match">
+        <div className="match" onClick={handleMatch}>
           <img src={heart} alt="" />
         </div>
       </div>
+      <Link to="/countries">
+        <button type="button" onClick={handleclick}>
+          Start
+        </button>
+      </Link>
     </div>
   );
 }
