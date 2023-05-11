1698b26c060a9699a0353891f2e885696c868a69
T0MLE <lelaurain.tom@gmail.com>
2023-05-11 21:41:04 +0200

    fixed css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,4 @@
 import { Routes, Route } from "react-router-dom";
-import "./App.css";
 import Countries from "./pages/Countries";
 import Home from "./pages/Home";
 import Region from "./pages/Region";
