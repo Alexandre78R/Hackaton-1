87f7fe5d4c54fde9d5cb9dad7bb6297ed3b1488b
Kader B <kder000@gmail.com>
2023-05-11 18:57:00 +0200

    force

--- src/components/ListCountries.jsx
+++ src/components/ListCountries.jsx
@@ -8,7 +8,6 @@ function ListCountries() {
             <div className="favpictures">
                 <img src="../src/assets/photo1.jpg" alt="" width="80%"/>
                 <p><img src="../src/assets/localisation.png" alt="" width="20px"/>Ville, PAYS</p>
-            
             </div>
         </>
     );
