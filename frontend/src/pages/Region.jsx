355f2dc0269a2b2e4649802b6b0fd8f41685cb20
T0MLE <lelaurain.tom@gmail.com>
2023-05-11 17:38:28 +0200

    created frontend folder

---
+++ frontend/src/pages/Region.jsx
@@ -0,0 +1,95 @@
+import { useEffect, useState } from "react";
+
+function Region() {
+  const [selectedRegions, setSelectedRegions] = useState([]);
+
+  const handleClickTheme = (name) => {
+    const searchSelectRegion = selectedRegions.find((e) => e.region === name);
+    if (searchSelectRegion !== undefined) {
+      setSelectedRegions((prevState) => [
+        ...prevState.filter((theme) => theme.region !== name),
+      ]);
+      return;
+    }
+    setSelectedRegions((prevState) => [...prevState, { region: name }]);
+  };
+
+  useEffect(() => {
+    console.info("selectedRegions", selectedRegions);
+  }, [selectedRegions]);
+
+  return (
+    <div>
+      <button
+        type="button"
+        onClick={() => handleClickTheme("Europe")}
+        style={{
+          backgroundColor:
+            selectedRegions.find((name) => name.region === "Europe") !==
+            undefined
+              ? "Red"
+              : "gray",
+        }}
+      >
+        Europe
+      </button>
+      <button
+        type="button"
+        onClick={() => handleClickTheme("Asia")}
+        style={{
+          backgroundColor:
+            selectedRegions.find((name) => name.region === "Asia") !== undefined
+              ? "Red"
+              : "gray",
+        }}
+      >
+        Asia
+      </button>
+      <button
+        type="button"
+        onClick={() => handleClickTheme("Oceania")}
+        style={{
+          backgroundColor:
+            selectedRegions.find((name) => name.region === "Oceania") !==
+            undefined
+              ? "Red"
+              : "gray",
+        }}
+      >
+        Oceania
+      </button>
+      <button
+        type="button"
+        onClick={() => handleClickTheme("Africa")}
+        style={{
+          backgroundColor:
+            selectedRegions.find((name) => name.region === "Africa") !==
+            undefined
+              ? "Red"
+              : "gray",
+        }}
+      >
+        Africa
+      </button>
+      <button
+        type="button"
+        onClick={() => handleClickTheme("America")}
+        style={{
+          backgroundColor:
+            selectedRegions.find((name) => name.region === "America") !==
+            undefined
+              ? "Red"
+              : "gray",
+        }}
+      >
+        America
+      </button>
+      <p>Sélection : </p>
+      {selectedRegions.map((region) => {
+        return <h1 key={region.region}> {region.region}</h1>;
+      })}
+    </div>
+  );
+}
+
+export default Region;
